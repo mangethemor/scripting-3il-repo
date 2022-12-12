94aa3185cf48622941ec592d8098d52dd9848ccc
Kimi no namae wa <potdeuxfleur@fleur.com>
2022-12-12 16:49:34 +0100

    Update

--- paramiko.py
+++
@@ -1,8 +0,0 @@
-import paramiko
-ssh = paramiko.SSHClient()
-
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect('192.168.0.26', username='tapha', key_filename="C:\\Users\\Pot2Fleur\\.ssh\\id_rsa")
-stdin, stdout, stderr = ssh.exec_command('ls -l')
-print(stdout.readlines())
-ssh.close()
--- testssh.py
+++ testssh.py
@@ -3,6 +3,13 @@ ssh = paramiko.SSHClient()
 
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect('192.168.0.26', username='tapha', key_filename="C:\\Users\\Pot2Fleur\\.ssh\\id_rsa")
-stdin, stdout, stderr = ssh.exec_command('ls -l')
+
+sftp = ssh.open_sftp()
+# Upload
+filepath = "/home/tapha"
+localpath = "C:\\Users\\Pot2Fleur\\Desktop\\courspython.txt"
+sftp.put(localpath,filepath)
+
+stdin, stdout, stderr = ssh.exec_command('uptime')
 print(stdout.readlines())
 ssh.close()
