502b9a5a44c156f1bdc588dabd006e92ea31e8b9
Kimi no namae wa <potdeuxfleur@fleur.com>
2023-01-09 12:26:15 +0100

    add

--- testssh.py
+++ testssh.py
@@ -1,15 +1,22 @@
 import paramiko
 ssh = paramiko.SSHClient()
 
+#Pour éviter l'erreur "d'hôtes non reconnu"
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+#pour se connecter à la machine en ssh par une clé rsa
 ssh.connect('192.168.0.26', username='tapha', key_filename="C:\\Users\\Pot2Fleur\\.ssh\\id_rsa")
 
-sftp = ssh.open_sftp()
-# Upload
-filepath = "/home/tapha"
-localpath = "C:\\Users\\Pot2Fleur\\Desktop\\courspython.txt"
-sftp.put(localpath,filepath)
+# sftp = ssh.open_sftp()
+# # Upload
+# filepath = "/home/tapha"
+# localpath = "C:\\Users\\Pot2Fleur\\Desktop\\courspython.txt"
+# sftp.put(localpath,filepath)
 
-stdin, stdout, stderr = ssh.exec_command('uptime')
+#on lance la commande uptime et ce retour sera coupé en trois parties et stockés dans ces var
+stdin, stdout, stderr = ssh.exec_command('apt install -y apache2')
+#on affiche les lignes du retour
 print(stdout.readlines())
+
+#on ferme la connexion ssh
 ssh.close()
